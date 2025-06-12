3d86825fa6a891d58689f83156e271575eb0777b
AkshayKumar0399 <akshay.kumar.police.patil@gds.ey.com>
2025-06-12 10:08:53 +0530

    String Manupulation and Exception handling

---
+++ Exceptions.py
@@ -0,0 +1,11 @@
+
+itemsInCart = 0
+
+if itemsInCart != 2:
+    # raise Exception("Product card count not matching")
+    pass
+
+# assert(itemsInCart == 2)
+assert(itemsInCart == 0)
+
+
---
+++ StringDemo.py
@@ -0,0 +1,16 @@
+str = "RahulShettyAcademy.com"
+str1 = "firm"
+str2 = "com"
+
+# print(str[1]) # first char
+
+# print(str[0:5]) # Substring
+
+# print(str + " "+ str1) #Concatination
+
+# print(str2 in str) # Substring check
+
+# print(str.split("."))
+
+# strip is used to remove the  beginning or trailing white spaces
+# Examples: (strip(), lstrip(), rstrip())
