a232517312fba7dec1c07a8c9cbd82175af969a5
AkshayKumar0399 <akshay.kumar.police.patil@gds.ey.com>
2025-06-22 12:16:29 +0530

    handling exceptions

--- Exceptions.py
+++ Exceptions.py
@@ -8,4 +8,16 @@ if itemsInCart != 2:
 # assert(itemsInCart == 2)
 assert(itemsInCart == 0)
 
+# try:
+#     with open('readme.md','r') as reader:
+#         reader.read()
+# except:
+#     print("No such file found")
 
+try:
+    with open('text.py', 'r') as reader:
+        reader.read()
+except Exception as e:
+    print(e)
+finally:
+    print("cleaning process")
